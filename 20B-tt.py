12ec04db8647907baf5daa0abdf9a18d54570387
taehyun-joo <jth3585@naver.com>
2022-12-20 01:04:29 +0900

    2022-12-20

---
+++ 20B-tt.py
@@ -0,0 +1,22 @@
+import turtle as t
+
+#삼각형 그리기
+t.fd(100)
+t.lt(120)
+t.fd(100)
+t.lt(120)
+t.fd(100)
+t.lt(120)
+
+#사각형 그리기
+t.fd(100)
+t.lt(90)
+t.fd(100)
+t.lt(90)
+t.fd(100)
+t.lt(90)
+t.fd(100)
+t.lt(90)
+
+#원그리기
+t.circle(50)
